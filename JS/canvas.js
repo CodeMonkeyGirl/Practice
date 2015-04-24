f07f404b13f807ebc12bac7b924510eb39edbb93
MonkeyGirl <repunzle@gmail.com>
2015-04-24 13:53:43 -0500

    add fizzbuzz

--- JS/canvas.js
+++ JS/canvas.js
@@ -3,15 +3,15 @@ var context = my_canvas.getContext("2d");
 
 context.fillStyle = "yellow";
 context.beginPath();
-context.arc(90,50,50,.4*Math.PI,.05*Math.PI);
+context.arc(90, 50, 50, .4*Math.PI, .05*Math.PI);
 context.fill();
 context.fillStyle = "white";
 context.beginPath();
-context.arc(100,60,40,0,2*Math.PI);
+context.arc(100, 60, 40, 0, 2*Math.PI);
 context.fill();
 
 context.fillStyle = "grey";
-context.fillRect(160,130,40,70);
+context.fillRect(160, 130, 40, 70);
 
 context.font = "30px Garamond";
-context.fillText("Missing Cat",10,30);
+context.fillText("Missing Cat", 10, 30);
